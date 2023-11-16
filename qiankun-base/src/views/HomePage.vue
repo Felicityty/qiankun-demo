8f42b60e4bf87a2f613dc24a98dd7563cd827247
suwan <suwan@tsign.cn>
2023-11-16 15:32:58 +0800

    scoped样式隔离

--- qiankun-base/src/views/HomePage.vue
+++ qiankun-base/src/views/HomePage.vue
@@ -12,3 +12,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+div {
+  color: red;
+}
+</style>
--- qiankun-sub1/src/App.vue
+++ qiankun-sub1/src/App.vue
@@ -14,7 +14,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 #app {
   height: 100%;
   width: 100%;
